e445b53965474fa73bc5c57cb38283d164cdc32c
rovinlr <rovinlr@gmail.com>
2026-02-24 18:51:47 -0600

    [IMP] cr_pos_einvoice: split invoice vs TE flow by invoice_status

--- cr_pos_einvoice/__manifest__.py
+++ cr_pos_einvoice/__manifest__.py
@@ -8,6 +8,7 @@
     "depends": ["point_of_sale", "account", "l10n_cr_einvoice"],
     "data": [
         "security/ir.model.access.csv",
+        "data/cron.xml",
         "views/pos_config_views.xml",
         "views/pos_payment_method_views.xml",
         "views/pos_order_views.xml",
