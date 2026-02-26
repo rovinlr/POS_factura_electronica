58f8979ba74554019307b736a1293ccd9ecff4d4
rovinlr <rovinlr@gmail.com>
2026-02-26 03:01:18 -0600

    Fix POS credit note reference document mapping

--- cr_pos_einvoice/tests/test_pos_einvoice.py
+++ cr_pos_einvoice/tests/test_pos_einvoice.py
@@ -125,3 +125,49 @@ class TestPosEInvoice(TransactionCase):
 
         self.assertTrue(synced)
         self.assertEqual(order.company_id.fp_consecutive_fe, "123")
+
+    def test_build_refund_reference_values_sets_reference_fields_when_available(self):
+        order = self.env["pos.order"].new({"company_id": self.env.company.id})
+        origin_invoice = self.env["account.move"].new(
+            {
+                "move_type": "out_invoice",
+                "name": "FAC-001",
+                "invoice_date": fields.Date.today(),
+            }
+        )
+
+        with patch.object(type(order), "_cr_get_origin_invoice_for_refund", lambda self: origin_invoice):
+            values = order._cr_build_refund_reference_values()
+
+        move_fields = self.env["account.move"]._fields
+        type_candidates = [
+            "fp_reference_document_type",
+            "fp_reference_document_code",
+            "fp_reference_doc_type",
+            "reference_document_type",
+        ]
+        number_candidates = [
+            "fp_reference_document_number",
+            "fp_reference_number",
+            "reference_document_number",
+            "reference_number",
+            "reversed_entry_number",
+        ]
+        date_candidates = [
+            "fp_reference_issue_date",
+            "fp_reference_document_date",
+            "fp_reference_date",
+            "reference_document_date",
+            "reference_date",
+            "reversed_entry_date",
+        ]
+
+        for field_name in type_candidates:
+            if field_name in move_fields:
+                self.assertEqual(values.get(field_name), "01")
+        for field_name in number_candidates:
+            if field_name in move_fields:
+                self.assertEqual(values.get(field_name), "FAC-001")
+        for field_name in date_candidates:
+            if field_name in move_fields:
+                self.assertEqual(values.get(field_name), origin_invoice.invoice_date)
